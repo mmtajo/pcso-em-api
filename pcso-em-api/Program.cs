c86a4e239d342d275d7ad8823c04c2ff24874e15
emtajo <mtajo@pcso.gov.ph>
2022-05-23 15:52:17 +0800

    Implemented intial post for Product and Chat

--- pcso-em-api/Program.cs
+++ pcso-em-api/Program.cs
@@ -10,48 +10,12 @@ app.UseHttpsRedirection();
 app.MapGet("Hello", () => "Hello from dotnet six API");
 app.MapGet("Product", () => "Get some Product from Azure SQL");
 
+app.MapPost("Product", (Product p) => "Product saved in SQL");
+app.MapPost("Chat", (Message msg) => "Message posted to chat SQL");
+
+
 app.Run();
 
-//var builder = WebApplication.CreateBuilder(args);
+public record class Message(string Name, string Msg);
 
-//// Add services to the container.
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseHttpsRedirection();
-
-//var summaries = new[]
-//{
-//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-//};
-
-//app.MapGet("/weatherforecast", () =>
-//{
-//    var forecast = Enumerable.Range(1, 5).Select(index =>
-//        new WeatherForecast
-//        (
-//            DateTime.Now.AddDays(index),
-//            Random.Shared.Next(-20, 55),
-//            summaries[Random.Shared.Next(summaries.Length)]
-//        ))
-//        .ToArray();
-//    return forecast;
-//})
-//.WithName("GetWeatherForecast");
-
-//app.Run();
-
-//internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-//{
-//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-//}
+public record class Product(string Name, string Picture, int Price);
